468862def8d98b8341e26debf6515e6dd3495d82
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2024-02-07 12:35:21 +0100

    Display result

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-        return `It's a tie`
+        return `It's a tie! ${playerSelection} ties with ${computerSelection}`
     } else if (
         (playerSelection === 'rock' && computerSelection === 'scissors') ||
         (playerSelection === 'scissors' && computerSelection === 'paper') ||
@@ -37,7 +37,30 @@ container.addEventListener('click', function (e) {
         const buttonText = e.target.textContent;
         const playerMove = buttonText.toLowerCase();
         const computerMove = getComputerChoice();
-        const result = playRound(playerMove, computerMove)
+        const result = playRound(playerMove, computerMove);
+        const playerScore = document.querySelector('#player');
+        const computerScore = document.querySelector('#computer');
+        const displayOne = document.querySelector('#displayOne');
+        const displayTwo = document.querySelector('#displayTwo');
+
+        let displayString = result.split('! ');
+
+        displayOne.textContent = displayString[0];
+        displayTwo.textContent = displayString[1];
+
+        playerScore.textContent = `Player ${player}`
+        computerScore.textContent = `Computer ${computer}`
+
+        if (player === 5 || computer === 5) {
+            let finalResult = `Game Over ${player === 5 ? 'You win' : 'You Lose'}`;
+            displayOne.textContent = finalResult;
+            displayTwo.textContent = 'Play Again';
+            playerScore.textContent = 'Player 0';
+            computerScore.textContent = 'Computer 0';
+            player = 0;
+            computer = 0;
+            return
+        }
     }
 })
 
